214cd32ef4680ea0d1f1afa765ffb26f07c94023
macstojak <40660610+macstojak@users.noreply.github.com>
2020-04-05 12:31:33 +0200

    update homework 2-2

--- homework2-2.js
+++ homework2-2.js
@@ -2,104 +2,102 @@
 // USE STRICT MODE
 // TRY TO BE OPTIMAL
 
-// 1)	Write a program that automatically converts English text to Morse code and vice versa.
-//-----------------------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------------------------------------------------
 // 2)	Write a program that finds the longest palindromic substring of a given string. ‘karakis’, ‘baerren’, ‘kajak’, ‘inni’.
 // //---------------------------------------------------------------------------------------------------------------------------
-"use strict";
-class Palindrome {
-  constructor(word) {
-    this.word = word;
-    this.letters = [];
-    this.possibilities = [];
-  }
-  convertToString(letters) {
-    this.possibilities.push(letters.join(""));
-  }
-  convertToLettters() {
-    this.letters = this.word.split("");
-  }
-  theLongestPalindrome(){
-      return this.possibilities.reduce((a, b) => { return a.length > b.length ? a : b})
-  }
-  //Parzyste palindromy np. "inni"
-  checkEven() {
-    let result = [];
-    let array = this.letters;
-    let possibilities = [];
-    for (let i = 1; i < array.length - 1; i++) {
-      let begin = i - 1;
-      let middle1 = i;
-      let middle2 = i + 1;
-      let end = i + 2;
+// "use strict";
+// class Palindrome {
+//   constructor(word) {
+//     this.word = word;
+//     this.letters = [];
+//     this.possibilities = [];
+//   }
+//   convertToString(letters) {
+//     this.possibilities.push(letters.join(""));
+//   }
+//   convertToLettters() {
+//     this.letters = this.word.split("");
+//   }
+//   theLongestPalindrome(){
+//       return this.possibilities.reduce((a, b) => { return a.length > b.length ? a : b})
+//   }
+//   //Parzyste palindromy np. "inni"
+//   checkEven() {
+//     let result = [];
+//     let array = this.letters;
+//     let possibilities = [];
+//     for (let i = 1; i < array.length - 1; i++) {
+//       let begin = i - 1;
+//       let middle1 = i;
+//       let middle2 = i + 1;
+//       let end = i + 2;
 
-      while (begin >= 0 && end < array.length) {
-        if (array[middle1] === array[middle2]) {
-          if (result.length === 0) {
-            result.push(array[i], array[i+1]);
-          }
-          if(array[end]===array[begin]){
-            result.push(array[end]);
-            result.unshift(array[begin]);
-          }
+//       while (begin >= 0 && end < array.length) {
+//         if (array[middle1] === array[middle2]) {
+//           if (result.length === 0) {
+//             result.push(array[i], array[i+1]);
+//           }
+//           if(array[end]===array[begin]){
+//             result.push(array[end]);
+//             result.unshift(array[begin]);
+//           }
 
-          begin--;
-          end++;
-        } else {
-          result = [];
-          break;
-        }
-      }
-      if (result.length > 0) this.convertToString(result);
-    }
-  }
-  //Nieparzyste palindromy np. "kajak"
-  checkOdd() {
-    let result = [];
-    let array = this.letters;
-    let possibilities = [];
-    for (let i = 1; i < array.length - 1; i++) {
-      let begin = i - 1;
-      let middle = i;
-      let end = i + 1;
+//           begin--;
+//           end++;
+//         } else {
+//           result = [];
+//           break;
+//         }
+//       }
+//       if (result.length > 0) this.convertToString(result);
+//     }
+//   }
+//   //Nieparzyste palindromy np. "kajak"
+//   checkOdd() {
+//     let result = [];
+//     let array = this.letters;
+//     let possibilities = [];
+//     for (let i = 1; i < array.length - 1; i++) {
+//       let begin = i - 1;
+//       let middle = i;
+//       let end = i + 1;
 
-      while (begin >= 0 && end < array.length) {
-        if (array[middle] === array[end]) {
-          if (result.length === 0) {
-            result.push(array[middle]);
-          }
+//       while (begin >= 0 && end < array.length) {
+//         if (array[middle] === array[end]) {
+//           if (result.length === 0) {
+//             result.push(array[middle]);
+//           }
 
-          result.push(array[end]);
-          result.unshift(array[begin]);
-          begin--;
-          end = end + 2;
-        }
-        if (array[begin] === array[end]) {
-          if (result.length === 0) {
-            result.push(array[i]);
-          }
-          result.push(array[end]);
-          result.unshift(array[begin]);
-          begin--;
-          end++;
-        } else {
-          result = [];
-          break;
-        }
-      }
-      if (result.length > 0) this.convertToString(result);
-    }
-  }
-}
+//           result.push(array[end]);
+//           result.unshift(array[begin]);
+//           begin--;
+//           end = end + 2;
+//         }
+//         if (array[begin] === array[end]) {
+//           if (result.length === 0) {
+//             result.push(array[i]);
+//           }
+//           result.push(array[end]);
+//           result.unshift(array[begin]);
+//           begin--;
+//           end++;
+//         } else {
+//           result = [];
+//           break;
+//         }
+//       }
+//       if (result.length > 0) this.convertToString(result);
+//     }
+//   }
+// }
 
-//Przykłady palindromów: "arakis", "kajak", "baerren", "inni",
-let palindrome = new Palindrome("arakis");
-let letters = palindrome.convertToLettters();
-palindrome.checkEven(letters);
-palindrome.checkOdd(letters);
-console.log("Najdłuższy palindrom:", JSON.stringify(palindrome.theLongestPalindrome()))
+// //Przykłady palindromów: "arakis", "kajak", "baerren", "inni",
+// let palindrome = new Palindrome("arakis");
+// let letters = palindrome.convertToLettters();
+// palindrome.checkEven(letters);
+// palindrome.checkOdd(letters);
+// console.log("Najdłuższy palindrom:", JSON.stringify(palindrome.theLongestPalindrome()))
 
 // 3)	Given two strings, write a program that efficiently finds the longest common subsequence. ‘karol rolki’
 //------------------------------------------------------------------------------------------------------------
@@ -140,13 +138,67 @@ console.log("Najdłuższy palindrom:", JSON.stringify(palindrome.theLongestPalin
 
 // 4)	Write a code that multiplies two matrices together. Dimension validation required.
 //---------------------------------------------------------------------------------------
-// ("use strict");
-// const a= [{0: 1, 1: 2}, {0:3, 1: 4}, {0: 9, 1:4}]
-// const b=[{0:5, 1:3, 2:5}, {0:9, 1:7, 2:5}, {0:3, 1:5, 2:7}]
+// "use strict"
+// const a=[
+//     [5, 3], 
+//     [3, 7]
+// ]
 
-// class Matrice{
-//     constructor(matrice){
-//         this.matrice= matrice;
+// const b= [  
+//             [2, 3], 
+//             [9, 1]
+//         ]
+
+// class Matrices{
+//     constructor(matrice1, matrice2){
+//         //A = n x m
+//         this.matrice1= matrice1;
+//         this.n=matrice1[0].length;
+//         this.m=matrice1.length;
+//         //B = m x p
+//         this.matrice2 = matrice2;
+//         this.p=matrice2.length;
+//         this.result=[[0,0],[0,0]];
 //     }
 
+
+//     checkDimensions(){
+//         let columnLength1 = this.matrice1.length;
+//         let rowLength2 = this.matrice2[0].length;
+//         let rowLength1 = this.matrice1[0].length
+//         let columnLength2 = this.matrice2.length;
+       
+//         let bool1 = columnLength1 === rowLength2;
+//         let bool2 = columnLength2 ===rowLength1;
+//         if(bool1&& bool2){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+
+  
+//     multiply(){
+//         let A = this.matrice1;
+//         let B = this.matrice2;
+//         for(let r = 0; r<this.n ; r++){
+           
+//             for(let i=0; i<this.n; i++){ 
+//                 for(let j=0; j<this.m; j++){  
+//                     this.result[r][i] += A[j][i]*B[r][j]
+//                 }
+//             }
+//         }
+//         return this.result;
+//     }
+    
+
 // }
+
+// let matrices = new Matrices(a,b);
+// let dimensionsCondition = matrices.checkDimensions();
+// console.log("Czy macierze mają równe wymiary?", dimensionsCondition);
+// if(dimensionsCondition){
+//     console.table(matrices.multiply());
+// }
+
